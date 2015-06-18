b60df60dd7e4cd79a67fe1631fbae6d240922e0f
eric-d-culver <eric.d.culver@gmail.com>
2015-06-17 21:14:36 -0600

    now it works with stdin, can pipe into it

--- ngram_count.py
+++ ngram_count.py
@@ -16,8 +16,8 @@ def print_ngrams(ngrams, n):
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Creates ngram probabilities for input file')
 	parser.add_argument('-b', dest='binary', action='store_const', const=ngrams_bin, default=ngrams_text, help='binary file (defaults: text file)')
-	parser.add_argument('filename', type=file, default=sys.stdin, help='name of file')
-	parser.add_argument('n', type=int, help='size of ngrams in characters')
+	parser.add_argument('-i', dest='filename', type=file, default=sys.stdin, help='name of file')
+	parser.add_argument('n', type=int, default=2, help='size of ngrams in characters')
 	args = parser.parse_args()
 	print_ngrams(args.binary(args.filename, args.n), args.n)
 
