0a90f9ae87009d764888d4872533fecccfd46cde
eric-d-culver <eric.d.culver@gmail.com>
2017-05-02 23:02:59 -0600

    converting tabs to spaces (Cython complained) and removing an unused function

--- demimicNgram.py
+++ demimicNgram.py
@@ -3,44 +3,38 @@ import fileinput, sys
 import bitFile
 import huffFileRead
 
-def getSymbols(info, prefix):
-	for thing in info:
-		if thing['prefix'] == prefix:
-			return thing['symbols']
-	return []
-
 def getCode(symbols, char):
-	for thing in symbols:
-		if thing['symbol'] == char:
-			return thing['code']
-	return []
+        for thing in symbols:
+                if thing['symbol'] == char:
+                        return thing['code']
+        return []
 
 def decode(infile, outfile, info, n): # write a file bit by bit using codes in info
-	output = bitFile.bitWrite(outfile)
-	prefix = list(infile.read(n-1)) # prefix is first n-1 characters (the seed)
-	char = infile.read(1)
-	while char:
-		symbols = info[''.join(prefix)]
-		code = getCode(symbols, char) # find code corresponding to prefix and next char
-		output.write(code) # write those bits to outfile
-		prefix.append(char) # shift prefix
-		prefix = prefix[1:]
-		char = infile.read(1) # read next char
+        output = bitFile.bitWrite(outfile)
+        prefix = list(infile.read(n-1)) # prefix is first n-1 characters (the seed)
+        char = infile.read(1)
+        while char:
+                symbols = info[''.join(prefix)]
+                code = getCode(symbols, char) # find code corresponding to prefix and next char
+                output.write(code) # write those bits to outfile
+                prefix.append(char) # shift prefix
+                prefix = prefix[1:]
+                char = infile.read(1) # read next char
 
 if __name__ == "__main__": # Stdout should be the file that is being hidden, stdin is file mimicing freqs of huffman file
-	if len(sys.argv) > 2:
-		infofile = open(sys.argv[2], 'r')
-		infile = open(sys.argv[1], 'r')
-	elif len(sys.argv) > 1:
-		infofile = open("huffman5.txt", 'r')
-		infile = open(sys.argv[1], 'r')
-	else:
-		infofile = open("huffman5.txt", 'r')
-		infile = sys.stdin
+        if len(sys.argv) > 2:
+                infofile = open(sys.argv[2], 'r')
+                infile = open(sys.argv[1], 'r')
+        elif len(sys.argv) > 1:
+                infofile = open("huffman5.txt", 'r')
+                infile = open(sys.argv[1], 'r')
+        else:
+                infofile = open("huffman5.txt", 'r')
+                infile = sys.stdin
 
         outfile = sys.stdout
         n, info = huffFileRead.huffRead(infofile)
         #print info
-	decode(infile, outfile, info, n)
-	infofile.close()
-	infile.close()
+        decode(infile, outfile, info, n)
+        infofile.close()
+        infile.close()
--- mimicNgram.py
+++ mimicNgram.py
@@ -4,47 +4,47 @@ import bitFile
 import huffFileRead
 
 def encode(infile, outfile, info, seed = ''): # read file bit by bit and encode using codes in info into output stream
-	inBits = bitFile.bitRead(infile)
-	outfile.write(seed)
-	prefix = list(seed)
-	while len(inBits.peek(1)) != 0:
-		symbols = info[''.join(prefix)]
-		# using symbols and bits from input.read(), determine what the next symbol is
-		nextChar = ''
-		for thing in symbols:
-			if inBits.peek(thing['length']) == thing['code']:
+        inBits = bitFile.bitRead(infile)
+        outfile.write(seed)
+        prefix = list(seed)
+        while len(inBits.peek(1)) != 0:
+                symbols = info[''.join(prefix)]
+                # using symbols and bits from input.read(), determine what the next symbol is
+                nextChar = ''
+                for thing in symbols:
+                        if inBits.peek(thing['length']) == thing['code']:
                                 inBits.read(thing['length'])
-				nextChar = thing['symbol']
+                                nextChar = thing['symbol']
                                 break
                 if nextChar == '':
                     nextChar = ' ' # if the prefix does not occur in huffman file, add a space
-		# write next symbol to output and shift prefix
-		outfile.write(nextChar)
-		prefix.append(nextChar)
-		prefix = prefix[1:]
+                # write next symbol to output and shift prefix
+                outfile.write(nextChar)
+                prefix.append(nextChar)
+                prefix = prefix[1:]
 
 if __name__ == "__main__": # Stdin should be the file that is being hidden, stdout is file mimicing freqs of huffman file
         if len(sys.argv) > 3:
                 prefix = sys.argv[3]
                 infofile = open(sys.argv[2], 'r')
                 infile = open(sys.argv[1], 'r')
-	elif len(sys.argv) > 2:
+        elif len(sys.argv) > 2:
                 prefix = "Shall I c"
-		infofile = open(sys.argv[2], 'r')
-		infile = open(sys.argv[1], 'r')
-	elif len(sys.argv) > 1:
+                infofile = open(sys.argv[2], 'r')
+                infile = open(sys.argv[1], 'r')
+        elif len(sys.argv) > 1:
                 prefix = "Shall I c"
-		infofile = open("huffman10.txt", 'r')
-		infile = open(sys.argv[1], 'r')
-	else:
+                infofile = open("huffman10.txt", 'r')
+                infile = open(sys.argv[1], 'r')
+        else:
                 prefix = "Shall I c"
-		infofile = open("huffman10.txt", 'r')
-		infile = sys.stdin
+                infofile = open("huffman10.txt", 'r')
+                infile = sys.stdin
 
         #outfile = open("play10.txt", 'w')
         outfile = sys.stdout
         n, info = huffFileRead.huffRead(infofile)
         #print info
-	encode(infile, outfile, info, prefix)
-	infofile.close()
-	infile.close()
+        encode(infile, outfile, info, prefix)
+        infofile.close()
+        infile.close()
