a825a94f2f26fa446c3a20df4dd1eb9e3f0c1465
eric-d-culver <eric.d.culver@gmail.com>
2017-05-08 23:10:25 -0600

    creating dynamic arrays and boilerplate code

--- bitFile.c
+++ bitFile.c
@@ -23,16 +23,73 @@ unsigned char bitsToByte(unsigned char* bits) {
 				return byte;
 }
 
+dynaArray* initArray() { /* initilizes dynamic array */
+				dynaArray* self = malloc(sizeof(dynaArray));
+				self->num_elements = 0;
+				self->num_allocated = 3;
+				
+				self->array = malloc((self->num_allocated) * sizeof(unsigned char));
+
+				return self;
+}
+
+int addToArray(dynaArray* self, unsigned char item) { /* adds item to end of dynamic array self */
+				if((self->num_elements) == (self->num_allocated)) { /* are more refs required? */
+								if (self->num_allocated == 0) {
+												self->num_allocated = 3; /* start off with 3 */
+								} else {
+												self->num_allocated *= 2; /* double the number of refs */
+								}
+								self->array = realloc(self->array, (num_allocated * sizeof(unsigned char)));
+				}
+
+				(self->array)[self->num_elements] = item;
+				(self->num_elements)++;
+
+				return 0;
+}
+
+int removeFromArray(dynaArray* self, int numItems) { /* removes numItems from beginning of dynamic array self */
+				memmove(self->array, (self->array)+numItems, (self->num_elements)-numItems);
+				(self->num_elements) -= numItems;
+				return 0;
+}
+
+void finiArray(dynaArray* self) { /* empties and frees dynamic array self */
+				removeFromArray(self, self->num_elements);
+				free(self->array);
+				self->num_elements = 0;
+				self->num_allocated = 0;
+				free(self);
+				return;
+}
+
 bitRead* initBitRead(FILE* file) {
 				bitRead* self = malloc(sizeof(bitRead));
 				self->file = file;
+				self->bits = initArray();
 				return self;
 }
 
 bitWrite* initBitWrite(FILE* file) {
 				bitWrite* self = malloc(sizeof(bitWrite));
 				self->file = file;
+				self->bits = initArray();
 				return self;
+}
+
+FILE* finiBitRead(bitRead* self) {
+				FILE* tmp = self->file;
+				finiArray(self->bits);
+
+				return tmp;
+}
+
+FILE* finiBitWrite(bitWrite* self) {
+				FILE* tmp = self->file;
+				finiArray(self->bits);
+
+				return tmp;
 }
 
 /*
--- bitFile.h
+++ bitFile.h
@@ -8,8 +8,14 @@ unsigned char* byteToBits(unsigned char byte);
 unsigned char bitsToByte(unsigned char* bits);
 
 typedef struct {
+				unsigned char* array;
+				int num_elements;
+				int num_allocated;
+} dynaArray;
+
+typedef struct {
 				FILE* file;
-				unsigned char* bits;
+				dynaArray* bits;
 				unsigned char* (*read)(const void* self, int numBits);
 				unsigned char* (*peek)(const void* self, int numBits);
 				int (*isEOF)(const void* self);
@@ -17,11 +23,13 @@ typedef struct {
 
 typedef struct {
 				FILE* file;
-				unsigned char* bits;
+				dynaArray* bits;
 				void (*write)(const void* self, unsigned char* bits);
 } bitWrite;
 
 bitRead* initBitRead(FILE* file);
 bitWrite* initBitWrite(FILE* file);
+FILE* finiBitRead(bitRead* self);
+FILE* finiBitWrite(bitWrite* self);
 
 #endif
